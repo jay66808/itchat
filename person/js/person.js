a054b23b24078329d1e7902c90e0d30809c38c49
jay66808 <503305904@qq.com>
2016-06-02 14:34:38 +0000

    Add files via upload

---
+++ person/js/person.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+	$(".information").click(function(){
+		$(".information").css("background","rgba(0,0,0,0)");
+	
+		$(".second").css("display","none");
+		$(this).css("background-color","#DDDDDD");
+
+	});
+	$("#tips").click(function(){
+	$("#tiezi").css("display","block");
+
+});
+	$("#photograph").click(function(){
+	$("#xiangce").css("display","block");
+});
+	$("#theme").click(function(){
+	$("#zhuti").css("display","block");
+});
+
+});
